cf0491bc89e4f6084cd1fbc5707d4d026f78db19
Unknown <tuan.nguyen@sotatek.com>
2018-03-29 15:20:50 +0700

    save transfer event

---
+++ smart-contract.py
@@ -0,0 +1,102 @@
+"""
+Example of running a NEO node and receiving notifications when events
+of a specific smart contract happen.
+
+Events include Runtime.Notify, Runtime.Log, Storage.*, Execution.Success
+and several more. See the documentation here:
+
+http://neo-python.readthedocs.io/en/latest/smartcontracts.html
+"""
+import threading
+from time import sleep
+
+from logzero import logger
+from twisted.internet import reactor, task
+from neocore.UInt160 import UInt160
+
+from neo.contrib.smartcontract import SmartContract
+from neo.Network.NodeLeader import NodeLeader
+from neo.Core.Blockchain import Blockchain
+from neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain import LevelDBBlockchain
+from neo.Settings import settings
+import mysql.connector
+
+config = {
+  'user': 'root',
+  'password': '123456',
+  'host': 'localhost',
+  'database': 'CGE_transfer',
+  'raise_on_warnings': True,
+}
+
+cnx = mysql.connector.connect(**config)
+cursor = cnx.cursor()
+add_transfer = ("INSERT INTO transfer_event (fromAddress, toAddress, value) VALUES (%s, %s, %s)")
+# If you want the log messages to also be saved in a logfile, enable the
+# next line. This configures a logfile with max 10 MB and 3 rotations:
+settings.set_logfile("/tmp/logfile.log", max_bytes=1e7, backup_count=3)
+
+# Setup the smart contract instance
+smart_contract = SmartContract("34579e4614ac1a7bd295372d3de8621770c76cdc")
+
+
+# Register an event handler for Runtime.Notify events of the smart contract.
+@smart_contract.on_notify
+def sc_notify(event):
+    logger.info("SmartContract Runtime.Notify event: %s", event)
+
+    # Make sure that the event payload list has at least one element.
+    if not len(event.event_payload):
+        return
+
+    # The event payload list has at least one element. As developer of the smart contract
+    # you should know what data-type is in the bytes, and how to decode it. In this example,
+    # it's just a string, so we decode it with utf-8:
+    eventType = event.event_payload[0].decode("utf-8")
+    if eventType == "tranfer":
+        from_addr = UInt160(data=event.event_payload[1])
+        to_addr = UInt160(data=event.event_payload[2])
+        value = int.from_bytes(event.event_payload[3], 'little')
+        data_transfer = (from_addr, to_addr, value)
+        cursor.execute(add_transfer, data_transfer)
+        cnx.commit()
+    logger.info("- payload part 1: %s", event.event_payload[0].decode("utf-8"))
+
+
+def custom_background_code():
+    """ Custom code run in a background thread. Prints the current block height.
+
+    This function is run in a daemonized thread, which means it can be instantly killed at any
+    moment, whenever the main thread quits. If you need more safety, don't use a  daemonized
+    thread and handle exiting this thread in another way (eg. with signals and events).
+    """
+    while True:
+        logger.info("Block %s / %s", str(Blockchain.Default().Height), str(Blockchain.Default().HeaderHeight))
+        sleep(15)
+
+
+def main():
+    # Setup the blockchain
+    blockchain = LevelDBBlockchain(settings.chain_leveldb_path)
+    Blockchain.RegisterBlockchain(blockchain)
+    dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
+    dbloop.start(.1)
+    NodeLeader.Instance().Start()
+
+    # Disable smart contract events for external smart contracts
+    settings.set_log_smart_contract_events(False)
+
+    # Start a thread with custom code
+    d = threading.Thread(target=custom_background_code)
+    d.setDaemon(True)  # daemonizing the thread will kill it when the main thread is quit
+    d.start()
+
+    # Run all the things (blocking call)
+    logger.info("Everything setup and running. Waiting for events...")
+    reactor.run()
+    logger.info("Shutting down.")
+
+
+if __name__ == "__main__":
+    main()
+
