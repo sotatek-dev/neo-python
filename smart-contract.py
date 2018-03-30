a93df5dcb0ec8d3cbb752e4465435c43d8834313
An Nguyen <nguyenhuuan@gmail.com>
2018-03-31 02:18:51 +0700

    Trying to process next block when there's exception.

--- smart-contract.py
+++ smart-contract.py
@@ -91,7 +91,10 @@ def main():
     # Setup the blockchain
     blockchain = LevelDBBlockchain(settings.chain_leveldb_path)
     Blockchain.RegisterBlockchain(blockchain)
-    dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
+    try
+        dbloop = task.LoopingCall(Blockchain.Default().PersistBlocks)
+    except Exception as e:
+        logger.info("Ignore error to continue: %s " % e)
     dbloop.start(.1)
     NodeLeader.Instance().Start()
 
